d08e9043716831ced36e672957d228e199d4714b
Kyle Cubit <kjc70@students.uwf.edu>
2024-08-04 22:24:34 -0500

    Implement push notification base

--- App.js
+++ App.js
@@ -1,35 +1,40 @@
 
-import React from "react";
-import { NavigationContainer } from '@react-navigation/native';
-import LoginScreen from "Containers/LoginScreen";
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-const Stack = createNativeStackNavigator();
-import HomeScreen from "Containers/HomeScreen";
-import SignUpScreen from "Containers/SignUpScreen";
-import ListScreen from "Containers/ListScreen";
-import SublistDetailScreen from "Containers/SublistDetailScreen";
-import { Provider } from "react-redux";
-import store, { persistor } from "Redux/Store";
+import { useState, useEffect, useRef, React } from 'react';
+import { Text } from 'react-native';
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { PersistGate } from 'redux-persist/integration/react'
-import { Text } from "react-native";
+import { Provider } from "react-redux";
+import store, { persistor } from "Redux/Store";
+import * as Notifications from 'expo-notifications';
+import { initNotifications } from "Notifications";
+import MainNavigator from "Components/Navigators/MainNavigator";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
 
 export default function App() {
+
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [channels, setChannels] = useState([]);
+  const [notification, setNotification] = useState();
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    initNotifications(notificationListener, responseListener, setExpoPushToken, setNotification)
+  }, []);
+
   return (
     <GestureHandlerRootView>
-
       <Provider store={store}>
         <PersistGate loading={<Text>Loading...</Text>} persistor={persistor}>
-
-          <NavigationContainer >
-            <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="LoginScreen"  >
-              <Stack.Screen name="LoginScreen" component={LoginScreen} />
-              <Stack.Screen name="HomeScreen" component={HomeScreen} />
-              <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
-              <Stack.Screen name="ListScreen" component={ListScreen} />
-              <Stack.Screen name="SublistDetailScreen" component={SublistDetailScreen} />
-            </Stack.Navigator>
-          </NavigationContainer>
+          <MainNavigator />
         </PersistGate>
       </Provider>
     </GestureHandlerRootView>
