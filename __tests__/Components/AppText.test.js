66fd341e1f01150e008823fe2e808303b4730cae
Kyle Cubit <kjc70@students.uwf.edu>
2024-08-10 15:52:54 -0500

    Successfully added packages for testing

--- __tests__/Components/AppText.test.js
+++ __tests__/Components/AppText.test.js
@@ -1,6 +1,6 @@
 import { ScreenDimensions } from "util";
 import AppText from "Components/AppText";
-
+import { render } from "@testing-library/react-native"
 describe("AppText", () => {
     test("TEST1", () => {
         expect(1 == 1)
