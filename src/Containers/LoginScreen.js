4b71b7893132d1f9b2e35dcf27b72d450fca28e0
Kyle Cubit <kjc70@students.uwf.edu>
2024-08-13 00:42:50 -0500

    Added delete lists notifications where neccesary

--- src/Containers/LoginScreen.js
+++ src/Containers/LoginScreen.js
@@ -3,6 +3,7 @@ import { Image, View, StyleSheet, Button } from 'react-native';
 import AppButton from 'Components/Buttons/AppButton';
 import Screen from './Screen';
 import { useDispatch } from 'react-redux';
+import { cancelAllScheduledNotificationsAsync } from 'expo-notifications';
 function LoginScreen({ navigation }) {
     const dispatch = useDispatch()
     return (
@@ -10,7 +11,10 @@ function LoginScreen({ navigation }) {
             <Image style={{ top: 0, position: 'absolute' }} source={require("../../assets/LoginScreenBackground.jpeg")} />
             <View style={styles.buttonContainer}>
 
-                <AppButton title="Login" onPress={() => { dispatch({ type: "RESET_STATE" }) }} />
+                <AppButton title="Login" onPress={async () => {
+                    await cancelAllScheduledNotificationsAsync()
+                    dispatch({ type: "RESET_STATE" })
+                }} />
                 {/* <AppButton title="Login" onPress={() => { navigation.navigate("SignUpScreen") }} /> */}
                 <AppButton title="Skip" onPress={() => { navigation.navigate("HomeScreen") }} />
 
